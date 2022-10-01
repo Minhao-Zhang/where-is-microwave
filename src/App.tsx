0b5e4848fa4bf23cf40e4d85152c54c0ed813399
Minhao Zhang <zhangminhao12138@gmail.com>
2022-10-01 10:17:22 -0700

    added gps location for the current user

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L, { icon, LatLng } from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import './App.css';
 
 function App() {
   document.title = 'Where is Microwave';
   return (
     <div className="App">
+      {/* These are the required code to run leaflet map */}
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.1/dist/leaflet.css"
         integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
         crossOrigin=""/>
       <script src="https://unpkg.com/leaflet@1.9.1/dist/leaflet.js"
         integrity="sha256-NDI0K41gVbWqfkkaHj15IzU7PtMoelkzyKp8TOaFQ3s="
         crossOrigin=""></script>
-      
+    
+
       <MapContainer center={[47.653791, -482.307784]} zoom={16.5} scrollWheelZoom={true}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        <LocationMarker />
       </MapContainer>
     </div>
+  );
+}
+
+function LocationMarker() {
+  const [position, setPosition]= useState<LatLng | null>(null);
+  const [bbox, setBbox] = useState<string[]>([]);
+
+  const map = useMap();
+
+  useEffect(() => {
+    map.locate().on("locationfound", function (e) {
+      setPosition(e.latlng);
+      // map.flyTo(e.latlng, map.getZoom());
+      map.setView(e.latlng, map.getZoom());
+      const radius = e.accuracy;
+      const circle = L.circle(e.latlng, radius);
+      circle.addTo(map);
+      setBbox(e.bounds.toBBoxString().split(","));
+    });
+  }, [map]);
+
+  return position === null ? null : (
+    <Marker position={position} >
+      <Popup>
+        You are here. <br />
+        Map bbox: <br />
+        <b>Southwest lng</b>: {bbox[0]} <br />
+        <b>Southwest lat</b>: {bbox[1]} <br />
+        <b>Northeast lng</b>: {bbox[2]} <br />
+        <b>Northeast lat</b>: {bbox[3]}
+      </Popup>
+    </Marker>
   );
 }
 
